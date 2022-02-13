90cef7a0106977b638f372795939d5c0aca59e5b
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-02-12 23:01:52 -0500

    :sparkles: [ADD] provider store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { StoreProvider } from '../context/storeContext';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
