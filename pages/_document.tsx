5c3b896ea2f62574e7f26375ab96224fc8597009
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-02-18 11:39:34 -0500

    :lipstick: [ADD] custom documen

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
-    <Html>
+    <Html lang="es">
       <Head>
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Patua+One&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Patua+One&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <div id='modal-root'></div>
-        <div id='modal-cart'></div>
+        <div id="modal-root"></div>
+        <div id="modal-cart"></div>
       </body>
     </Html>
   );
